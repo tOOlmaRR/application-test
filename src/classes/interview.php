9d7e011b4e0b0adb8c4336010d0d1f3974c9256e
tOOlmaRR <georgeumarr@hotmail.com>
2020-06-11 17:52:45 -0400

    display the user's name when their button is clicked

---
+++ src/classes/interview.php
@@ -0,0 +1,13 @@
+<?php
+namespace InterviewExercise;
+
+class People
+{
+    public static $allPeople = array(
+        array('id'=>1, 'first_name'=>'John', 'last_name'=>'Smith', 'email'=>'john.smith@hotmail.com'),
+        array('id'=>2, 'first_name'=>'Paul', 'last_name'=>'Allen', 'email'=>'paul.allen@microsoft.com'),
+        array('id'=>3, 'first_name'=>'James', 'last_name'=>'Johnston', 'email'=>'james.johnston@gmail.com'),
+        array('id'=>4, 'first_name'=>'Steve', 'last_name'=>'Buscemi', 'email'=>'steve.buscemi@yahoo.com'),
+        array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
+    );
+}
