d3ae91433902c89c0f62ab1b5c181c5cad125090
tOOlmaRR <georgeumarr@hotmail.com>
2020-06-11 16:30:59 -0400

    display table of people
    loops through the static list of people and display them in a table

--- index.php
+++ index.php
@@ -1,1 +1,53 @@
-Place your code here
+<?php
+namespace eBaseInterviewExercise;
+
+class Interview
+{
+    public static $title = 'Interview test';
+    
+    public static $people = array(
+        array('id'=>1, 'first_name'=>'John', 'last_name'=>'Smith', 'email'=>'john.smith@hotmail.com'),
+        array('id'=>2, 'first_name'=>'Paul', 'last_name'=>'Allen', 'email'=>'paul.allen@microsoft.com'),
+        array('id'=>3, 'first_name'=>'James', 'last_name'=>'Johnston', 'email'=>'james.johnston@gmail.com'),
+        array('id'=>4, 'first_name'=>'Steve', 'last_name'=>'Buscemi', 'email'=>'steve.buscemi@yahoo.com'),
+        array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
+    );
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Interview test</title>
+    <style>
+        body {font: normal 14px/1.5 sans-serif;}
+    </style>
+</head>
+<body>
+
+    <h1><?=Interview::$title;?></h1>
+
+    <hr>
+
+    <table>
+        <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach (Interview::$people as $person) : ?>
+                <tr>
+                    <td><?=$person['first_name'];?></td>
+                    <td><?=$person['last_name'];?></td>
+                    <td><?=$person['email'];?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+</body>
+</html>
