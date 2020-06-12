7aadff3c02064e6c3d22932264dba14c3f195650
tOOlmaRR <georgeumarr@hotmail.com>
2020-06-12 10:52:07 -0400

    alter Angular implementation slightly
    allow the controller to receive a scope variable in it's constructor to bind the model, view, and controller together

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
-angular.module('PeopleListApp', [])
-  .controller('PeopleListController', function() {
-    var listOfPeople = this;
-    listOfPeople.people = [
+let peopleListApp = angular.module('peopleListApp', []);
+peopleListApp.controller('peopleListController', function($scope) {
+    $scope.people = [
       {id:1, firstName:'John', lastName:'Smith', email:'john.smith@hotmail.com'},
       {id:2, firstName:'Paul', lastName:'Allen', email:'paul.allen@microsoft.com'},
       {id:3, firstName:'James', lastName:'Johnston', email:'james.johnston@gmail.com'},
@@ -9,7 +8,7 @@ angular.module('PeopleListApp', [])
       {id:5, firstName:'Doug', lastName:'Simons', email:'doug.simons@hotmail.com'},
     ];
 
-    listOfPeople.alertEmail = function(email) {
+    $scope.alertEmail = function(email) {
       alert(email);
     };
   });
