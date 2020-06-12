80e8e66eadcbcf84287cbe81cb9e4ed25f33f36c
tOOlmaRR <georgeumarr@hotmail.com>
2020-06-12 10:19:27 -0400

    implement People table using Angular JS
    NOTE: the main page has been renamed to 'index.html' since there is no PHP in this version. Functionality is the same as v1 but now using Angular JS instead of PHP to dynamically populate the table and the button alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,15 @@
-function displayEmail(email) {
-    message = `${email}`;
-    alert(message);
-}
+angular.module('PeopleListApp', [])
+  .controller('PeopleListController', function() {
+    var listOfPeople = this;
+    listOfPeople.people = [
+      {id:1, firstName:'John', lastName:'Smith', email:'john.smith@hotmail.com'},
+      {id:2, firstName:'Paul', lastName:'Allen', email:'paul.allen@microsoft.com'},
+      {id:3, firstName:'James', lastName:'Johnston', email:'james.johnston@gmail.com'},
+      {id:4, firstName:'Steve', lastName:'Buscemi', email:'steve.buscemi@yahoo.com'},
+      {id:5, firstName:'Doug', lastName:'Simons', email:'doug.simons@hotmail.com'},
+    ];
+
+    listOfPeople.alertEmail = function(email) {
+      alert(email);
+    };
+  });
